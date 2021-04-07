89ea101fb98fe772d38a4cc19a706d121760fb2c
Pablo Sazo <pablo@ripehouseadvisory.com.au>
2021-04-07 11:30:48 +1000

    Lerning 01

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::get('cursos', [CursoController::class, 'index']);
+
+Route::get('cursos/create', [CursoController::class, 'create']);
+
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
+
+
